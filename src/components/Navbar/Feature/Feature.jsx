e81e133f14d64abe972c6458c2b9700d71fd36b6
sadiaprova12 <sadiaprova51717@gmail.com>
2024-03-28 23:17:53 +0600

    added features

---
+++ src/components/Navbar/Feature/Feature.jsx
@@ -0,0 +1,15 @@
+/* eslint-disable react/prop-types */
+// eslint-disable-next-line no-unused-vars
+import React from 'react';
+import { CheckCircleIcon } from '@heroicons/react/24/solid'
+
+const Feature = ({feature}) => {
+    return (
+        <div className='flex items-center'>
+            <CheckCircleIcon className="h-4 w-4 text-purple-600" />
+            <span className='pl-4'>{feature}</span>
+        </div>
+    );
+};
+
+export default Feature;
