0b46c5769786aae1402756fb2418847fb3d0bcc1
sadiaprova12 <sadiaprova51717@gmail.com>
2024-03-29 23:10:39 +0600

    import phone bar

--- src/App.jsx
+++ src/App.jsx
@@ -4,6 +4,7 @@ import './App.css'
 import Navbar from './components/Navbar/Navbar'
 import PriceList from './components/Navbar/PriceLists/PriceList'
 import Dashboard from './components/Navbar/Dashboard/Dashboard'
+import PhoneBar from './components/Navbar/PhoneBar/PhoneBar'
 
 function App() {
   const [count, setCount] = useState(0)
@@ -14,6 +15,7 @@ function App() {
       <h1 className='text-7xl text-purple-600'>Hello from tailwind</h1>
       <PriceList></PriceList>
       <Dashboard></Dashboard>
+      <PhoneBar></PhoneBar>
       
     </div>
   )
