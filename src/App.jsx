2238ac6937d8889d7f7c6ef8f052d7f1a801de96
sadiaprova12 <sadiaprova51717@gmail.com>
2024-03-29 22:49:57 +0600

    import dashboard

--- src/App.jsx
+++ src/App.jsx
@@ -3,6 +3,7 @@ import { useState } from 'react'
 import './App.css'
 import Navbar from './components/Navbar/Navbar'
 import PriceList from './components/Navbar/PriceLists/PriceList'
+import Dashboard from './components/Navbar/Dashboard/Dashboard'
 
 function App() {
   const [count, setCount] = useState(0)
@@ -12,6 +13,7 @@ function App() {
       <Navbar></Navbar>
       <h1 className='text-7xl text-purple-600'>Hello from tailwind</h1>
       <PriceList></PriceList>
+      <Dashboard></Dashboard>
       
     </div>
   )
